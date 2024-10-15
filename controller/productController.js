cf11a7e0f4b36106de67efb5863d7e4a1545a11e
Jake85631200 <Jake85631200@gmail.com>
2024-10-15 18:07:06 +0800

    Added try/catch

--- controller/productController.js
+++ controller/productController.js
@@ -1,57 +1,92 @@
 const Products = require("./../models/productModel");
 
 exports.getAllProducts = async (req, res, next) => {
-  const products = await Products.find();
-
-  res.status(200).json({
-    status: "success.",
-    result: products.length,
-    data: {
-      products,
-    },
-  });
+  try {
+    const products = await Products.find();
+    
+    res.status(200).json({
+      status: "success.",
+      result: products.length,
+      data: {
+        products,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
 exports.getProduct = async (req, res, next) => {
-  const product = await Products.findById(req.params.id);
-
-  res.status(200).json({
-    status: "success.",
-    data: {
-      product,
-    },
-  });
+  try {
+    const product = await Products.findById(req.params.id);
+    
+    res.status(200).json({
+      status: "success.",
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
 exports.updateProduct = async (req, res, next) => {
-  const product = await Products.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-
-  res.status(200).json({
-    status: "success.",
-    data: {
-      product,
-    },
-  });
+  try {
+    const product = await Products.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    
+    res.status(200).json({
+      status: "success.",
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
 exports.createProduct = async (req, res, next) => {
-  const product = await Products.create(req.body);
-
-  res.status(201).json({
-    status: "success.",
-    data: {
-      product,
-    },
-  });
+  try {
+    const product = await Products.create(req.body);
+    
+    res.status(201).json({
+      status: "success.",
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
 exports.deleteProduct = async (req, res, next) => {
-  const product = await Products.findByIdAndDelete(req.params.id);
-
-  res.status(204).json({
-    status: "success.",
-    data: null,
-  });
+  try {
+    const product = await Products.findByIdAndDelete(req.params.id);
+    
+    res.status(204).json({
+      status: "success.",
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
--- controller/userController.js
+++ controller/userController.js
@@ -1,53 +1,88 @@
 const Users = require("./../models/userModel");
 
 exports.getAllUsers = async (req, res, next) => {
-  const users = await Users.find();
+  try {
+    const users = await Users.find();
 
-  res.status(200).json({
-    status: "success.",
-    result: users.length,
-    data: users,
-  });
+    res.status(200).json({
+      status: "success.",
+      result: users.length,
+      data: users,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err
+    });
+  }
 };
 
 exports.getUser = async (req, res, next) => {
-  const user = await Users.findById(req.params.id);
+  try {
+    const user = await Users.findById(req.params.id);
 
-  res.status(200).json({
-    status: "success.",
-    data: {
-      user,
-    },
-  });
+    res.status(200).json({
+      status: "success.",
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err
+    });
+  }
 };
 
 exports.createUser = async (req, res, next) => {
-  const user = await Users.create(req.body);
+  try {
+    const user = await Users.create(req.body);
 
-  res.status(201).json({
-    status: "success.",
-    user,
-  });
+    res.status(201).json({
+      status: "success.",
+      user,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err
+    });
+  }
 };
 
 exports.updateUser = async (req, res, next) => {
-  const user = await Users.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
+  try {
+    const user = await Users.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
 
-  res.status(200).json({
-    status: "success.",
-    data: {
-      user,
-    },
-  });
+    res.status(200).json({
+      status: "success.",
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err
+    });
+  }
 };
 
 exports.deleteUser = async (req, res, next) => {
-  const user = await Users.findByIdAndDelete(req.params.id);
+  try {
+    const user = await Users.findByIdAndDelete(req.params.id);
 
-  res.status(204).json({
-    status: "success.",
-    data: null,
-  });
+    res.status(204).json({
+      status: "success.",
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err
+    });
+  }
 };
