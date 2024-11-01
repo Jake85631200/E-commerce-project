a13dd44d0a2d037e70afc76af198d4e2b969b2d2
Jake85631200 <Jake85631200@gmail.com>
2024-11-01 10:46:38 +0800

    Added protec, restirctTo middleware and disableProd route

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -8,12 +8,21 @@ const {
   createProduct,
   deleteProduct,
   updateProduct,
+  disableProd,
   productStats,
 } = require("./../controller/productController");
+
+const { protect, restrictTo } = require("./../controller/authController");
+
+router.use(protect);
 
 router.route("/productStats").get(productStats);
 
 router.route("/").get(getAllProducts).post(createProduct);
+
+router.use(restrictTo("admin"));
+
+router.patch("/disableProd/:id", disableProd);
 
 router.route("/:id").get(getProduct).patch(updateProduct).delete(deleteProduct);
 
