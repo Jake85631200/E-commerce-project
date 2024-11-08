e0b6d33588972e226e80066c643a4563c9e8d582
Jake85631200 <Jake85631200@gmail.com>
2024-11-08 22:10:45 +0800

    Added cart route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const globalErrorHandler = require("./controller/errorController.js");
 
 const userRouter = require("./routes/userRouter");
 const productRouter = require("./routes/productRouter");
+const cartRouter = require("./routes/cartRouter.js");
 
 const app = express();
 
@@ -24,8 +25,8 @@ if (process.env.NODE_ENV === "development") {
 // app.use("/");
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/cart", cartRouter);
 // app.use("/api/v1/reviews");
-// app.use("/api/v1/bookings");
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
