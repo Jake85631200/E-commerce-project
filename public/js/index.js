38171534973da2dd7cb0be401fdef31c261fd7c6
Jake85631200 <Jake85631200@gmail.com>
2024-12-11 00:44:30 +0800

    Implement myCart, search-bar function

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,14 @@
 import { login, logout } from "./login";
+import { myCart } from "./cart";
+import { search } from "./search";
 import { showAlert } from "./alerts";
 
 const loginForm = document.querySelector(".login-form");
 const logOutBtn = document.querySelector(".nav-logout-btn");
+const searchBarForm = document.querySelector(".search-bar-form");
+const productContainer = document.querySelector(".card-container");
+const searchInput = document.querySelector(".form-input");
+const myCartBtn = document.querySelector(".nav-cart");
 
 if (loginForm) {
   loginForm.addEventListener("submit", (e) => {
@@ -14,7 +20,25 @@ if (loginForm) {
 }
 
 if (logOutBtn) {
-  logOutBtn.addEventListener("click", logout);
+  logOutBtn.addEventListener("click", logout());
+}
+
+if (myCartBtn) {
+  myCartBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    myCart();
+  });
+}
+
+if (searchBarForm) {
+  searchBarForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    console.log(searchInput.value.toLowerCase());
+    const products = await search(searchInput.value.toLowerCase());
+    if (products) {
+      productContainer.innerHTML = products;
+    }
+  });
 }
 
 // IF dateset-alert = alert, showAlert
