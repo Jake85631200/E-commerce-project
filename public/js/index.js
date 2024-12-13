a0539cb5c9b2686af4b798689acd3a6375c679d8
Jake85631200 <Jake85631200@gmail.com>
2024-12-13 23:30:00 +0800

    Implement update user info and password request

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 import { login, logout } from "./login";
 import { myCart } from "./cart";
 import { search } from "./search";
+import { updateSettings } from "./updateSettings";
 import { showAlert } from "./alerts";
 
 const loginForm = document.querySelector(".login-form");
@@ -9,6 +10,8 @@ const searchBarForm = document.querySelector(".search-bar-form");
 const productContainer = document.querySelector(".card-container");
 const searchInput = document.querySelector(".form-input");
 const myCartBtn = document.querySelector(".nav-cart");
+const userDataForm = document.querySelector(".user-info-form");
+const userPasswordForm = document.querySelector(".user-password-form");
 
 if (loginForm) {
   loginForm.addEventListener("submit", (e) => {
@@ -20,7 +23,9 @@ if (loginForm) {
 }
 
 if (logOutBtn) {
-  logOutBtn.addEventListener("click", logout());
+  logOutBtn.addEventListener("click", (e) => {
+    logout();
+  });
 }
 
 if (myCartBtn) {
@@ -38,6 +43,45 @@ if (searchBarForm) {
     if (products) {
       productContainer.innerHTML = products;
     }
+  });
+}
+if (userDataForm) {
+  userDataForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const form = new FormData();
+    form.append("name", document.getElementById("name").value);
+    form.append("email", document.getElementById("email").value);
+    form.append("phone_number", document.getElementById("phone_number").value);
+    form.append("address", document.getElementById("address").value);
+
+    const selectedGender = document.querySelector('input[name="gender"]:checked');
+    if (selectedGender) {
+      form.append("gender", selectedGender.value);
+    }
+    
+    const selectedImage = document.getElementById("image").files[0];
+    if (selectedImage) {
+      form.append("image", document.getElementById("image").files[0]);
+    }
+
+    updateSettings(form, "data");
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    document.querySelector(".save-password").textContent = "Updating...";
+
+    const passwordCurrent = document.getElementById("password-current").value;
+    const password = document.getElementById("password").value;
+    const passwordConfirm = document.getElementById("password-confirm").value;
+    await updateSettings({ passwordCurrent, password, passwordConfirm }, "password");
+
+    document.querySelector(".save-password").textContent = "Save password";
+    document.getElementById("password-current").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("password-confirm").value = "";
   });
 }
 
