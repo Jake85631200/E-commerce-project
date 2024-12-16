7e4b935ca1d5478c6940c0a311b162a3ad65ad58
Jake85631200 <Jake85631200@gmail.com>
2024-12-16 21:22:52 +0800

    Deleted forgetPassword handler funnction and intergate twoFactor, verifyFACode, resetPassword as forget password procedure

--- controller/authController.js
+++ controller/authController.js
@@ -76,6 +76,16 @@ exports.signUp = catchAsync(async (req, res, next) => {
   createSendToken(newUser, 201, req, res);
 });
 
+exports.logout = (req, res) => {
+  // 以新 cookie "loggedout"，覆蓋 cookie "jwt"
+  res.cookie("jwt", "loggedout", {
+    // 10 秒後失效
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ status: "success" });
+};
+
 exports.login = catchAsync(async (req, res, next) => {
   // 1. Validate input data
   const { email, password } = req.body;
@@ -108,7 +118,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   // 6. Validate password
-  if (!(await user.correctPassword(password, user.password))) {
+  if (!(await user.comparePassword(password, user.password))) {
     return await handleFailedLogin(user, endOfDay, attempts);
   }
 
@@ -167,81 +177,25 @@ exports.verifyFACode = catchAsync(async (req, res, next) => {
   // Delete Verification code in redis
   await redis.del(`2fa_${email}`);
 
-  // Find user and login user
-  const user = await User.findOne({ email });
-  createSendToken(user, 200, req, res);
-});
-
-exports.logout = (req, res) => {
-  // 以新 cookie "loggedout"，覆蓋 cookie "jwt"
-  res.cookie("jwt", "loggedout", {
-    // 10 秒後失效
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true,
+  res.status(200).json({
+    status: "success",
   });
-  res.status(200).json({ status: "success" });
-};
-
-exports.forgetPassword = catchAsync(async (req, res, next) => {
-  // 1) Use email to confirm user
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) {
-    return next(new AppError("User not existed or invalid email.", 401));
-  }
-  // 2) Generate resetToken
-  const resetToken = user.createPasswordResetToken();
-  await user.save({ validateBeforeSave: false });
-
-  // 3) Send resetPassword URL within resetToken
-  try {
-    await sendMail(
-      "test1@gmail.com",
-      "test email",
-      `${req.protocol}://${req.get("host")}/api/v1/users/resetPassword/${resetToken}`,
-    );
-
-    res.status(200).send("Token sent to email!");
-  } catch (err) {
-    user.passwordResetToken = undefined;
-    user.passwordResetExpires = undefined;
-    await user.save({ validateBeforeSave: false });
-
-    return next(new AppError("Something wrong when sending email! Please try again later!", 500));
-  }
 });
 
 exports.resetPassword = catchAsync(async (req, res, next) => {
-  // 1) Get user based on the token
-  const hashedToken = crypto
-    .createHash("sha256") // Create a SHA-256 hash instance
-    .update(req.params.token) // Update the hash instance with the token from request parameters
-    .digest("hex"); // Convert the hash result to a hexadecimal string
+  const user = await User.findOne({ email: req.body.email }).select("+password");
 
-  const user = await User.findOne({
-    passwordResetToken: hashedToken,
-    passwordResetExpires: { $gt: Date.now() },
-  });
+  if (req.body.newPassword !== req.body.passwordConfirm)
+    return next(new AppError("Confirm password is not the same with password! Please try again!"), 401);
 
-  // 2) If token has not expired, and there is user, set the new password
-  if (!user) {
-    return next(new AppError("Invalid token or expired.", 401));
-  }
-  user.password = req.body.password;
+  if (await user.comparePassword(req.body.newPassword, user.password))
+    return next(new AppError("New password can't be the same as current password! Please try new one!"), 401);
+
+  user.password = req.body.newPassword;
   user.passwordConfirm = req.body.passwordConfirm;
-  // After resetting the password, token and expiration are no longer needed. Prevents user from using the same token to reset password again
-  user.passwordResetToken = undefined;
-  user.passwordResetExpires = undefined;
-
-  // 3) Update changedPasswordAt property for the user
-  User.passwordChangedAt = Date.now() - 1000;
-
-  // Remove login restrictions
-  user.loginAttempts = 0;
-  user.lockUntil = null;
 
   await user.save();
 
-  // 4) Log the user in, send JWT
   createSendToken(user, 200, req, res);
 });
 
@@ -251,7 +205,7 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   // Check if token is valid and if the user is logged in
   const user = await User.findById(req.user._id).select("+password");
 
-  if (!(await user.correctPassword(req.body.passwordCurrent, user.password)))
+  if (!(await user.comparePassword(req.body.passwordCurrent, user.password)))
     return next(new AppError("Your current password is incorrect! Please try again!"), 401);
   // Check if req.body contains valid password and passwordConfirm
   user.password = req.body.password;
