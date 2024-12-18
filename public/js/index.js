0bffc57341aabb0e4363e42fb7e3728f40614c05
Jake85631200 <Jake85631200@gmail.com>
2024-12-18 12:05:50 +0800

    Re-import and call functions

--- public/js/index.js
+++ public/js/index.js
@@ -1,141 +1,18 @@
-import { showAlert } from "./alerts";
-import { login, logout } from "./login";
-import { checkTwoFACode, sendTwoFACode, resetPassword } from "./twoFactorVerify";
-import { myCart } from "./cart";
-import { search } from "./search";
-import { updateSettings } from "./updateSettings";
+// IMPORT FUNCTIONS
+import { showAlert } from "./utils/alerts";
+import { initLoginOut } from "./user-auth/login";
+import { initForgetPassword } from "./user-auth/twoFactorVerify";
+import { initSearch } from "./search-item/search";
+import { initUpdateSetting } from "./user-account/updateSettings";
+import { initCart } from "./user-cart/cart";
 
-const loginForm = document.querySelector(".login-form");
-const logOutBtn = document.querySelector(".nav-logout-btn");
-const searchBarForm = document.querySelector(".search-bar-form");
-const productContainer = document.querySelector(".card-container");
-const searchInput = document.querySelector(".form-input");
-const myCartBtn = document.querySelector(".nav-cart");
-const userDataForm = document.querySelector(".user-info-form");
-const userPasswordForm = document.querySelector(".user-password-form");
-const sendEmailForm = document.querySelector(".send-email-form");
-const sendVerifyForm = document.querySelector(".send-verify-form");
-const resetPasswordForm = document.querySelector(".reset-password-form");
-
-const updateButtonText = (selector, text) => {
-  document.querySelector(selector).textContent = text;
-};
-
-if (loginForm) {
-  loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    login(email, password);
-  });
-}
-
-if (logOutBtn) {
-  logOutBtn.addEventListener("click", (e) => {
-    logout();
-  });
-}
-
-if (myCartBtn) {
-  myCartBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    myCart();
-  });
-}
-
-if (searchBarForm) {
-  searchBarForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const products = await search(searchInput.value.toLowerCase());
-    if (products) {
-      productContainer.innerHTML = products;
-    }
-  });
-}
-
-if (userDataForm) {
-  userDataForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const form = new FormData();
-    form.append("name", document.getElementById("name").value);
-    form.append("email", document.getElementById("email").value);
-    form.append("phone_number", document.getElementById("phone_number").value);
-    form.append("address", document.getElementById("address").value);
-
-    const selectedGender = document.querySelector('input[name="gender"]:checked');
-    if (selectedGender) {
-      form.append("gender", selectedGender.value);
-    }
-
-    const selectedImage = document.getElementById("image").files[0];
-    if (selectedImage) {
-      form.append("image", document.getElementById("image").files[0]);
-    }
-
-    updateSettings(form, "data");
-  });
-}
-
-if (userPasswordForm) {
-  userPasswordForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    updateButtonText(".save-password", "Updating...");
-
-    const passwordCurrent = document.getElementById("password-current").value;
-    const password = document.getElementById("password").value;
-    const passwordConfirm = document.getElementById("password-confirm").value;
-    await updateSettings({ passwordCurrent, password, passwordConfirm }, "password");
-
-    updateButtonText(".save-password", "Save password");
-    document.getElementById("password-current").value = "";
-    document.getElementById("password").value = "";
-    document.getElementById("password-confirm").value = "";
-  });
-}
-
-// Send verification code to email
-if (sendEmailForm) {
-  sendEmailForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    updateButtonText(".send-verification", "Sending...");
-    const email = document.getElementById("email").value;
-    if ((await sendTwoFACode(email)) === "success") {
-      document.getElementById("send-verify").style.display = "none";
-      document.getElementById("verify-email").style.display = "";
-    }
-    updateButtonText(".send-verification", "Send verification code");
-  });
-}
-
-// Confirm code form email
-if (sendVerifyForm) {
-  sendVerifyForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    updateButtonText(".code-verifying", "Verifying...");
-    const email = document.getElementById("email").value;
-    const verifyCode = document.getElementById("verify-code").value;
-
-    // If verification successful
-    if ((await checkTwoFACode(email, verifyCode)) === "success") {
-      document.getElementById("verify-email").style.display = "none";
-      document.getElementById("reset-password").style.display = "";
-    }
-    updateButtonText(".code-verifying", "Verify");
-  });
-}
-
-// Reset password
-if (resetPasswordForm) {
-  resetPasswordForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    const newPassword = document.getElementById("new-password").value;
-    const passwordConfirm = document.getElementById("password-confirm").value;
-
-    resetPassword(email, newPassword, passwordConfirm);
-  });
-}
+document.addEventListener("DOMContentLoaded", () => {
+  initLoginOut();
+  initForgetPassword();
+  initSearch();
+  initUpdateSetting();
+  initCart();
+});
 
 // IF dateset-alert = alert, showAlert
 const alertMsg = document.querySelector("body").dataset.alert;
