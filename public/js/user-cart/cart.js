9a62aa5fc8f037b80ba74216711a630d4939fc1f
Jake85631200 <Jake85631200@gmail.com>
2024-12-20 00:29:57 +0800

    Added checkedQty to store checked item quantity. Implement checkout buttom: deliver checked item and quantity then call orderProduct function.

--- public/js/user-cart/cart.js
+++ public/js/user-cart/cart.js
@@ -1,10 +1,13 @@
+import { showAlert } from "../utils/alerts";
 import {
   addToCartRequest,
   setQuantity,
   calCheckout,
   updateAllCheckbox,
   deleteItem,
-} from "../user-cart/cartFunction.js";
+  getCheckedItem,
+} from "../user-cart/cartFunction";
+import { orderProducts } from "../stripe";
 
 export const initCart = () => {
   const addToCart = document.querySelector(".add-to-cart-btn");
@@ -13,16 +16,21 @@ export const initCart = () => {
   const decreaseBtns = document.querySelectorAll(".decrease-btn");
   const increaseBtns = document.querySelectorAll(".increase-btn");
   const deleteItemBtn = document.querySelectorAll(".item-delete");
+  const checkoutBtn = document.querySelector(".checkout-btn");
+
+  let checkedItems = [];
+  let checkedQty = [];
 
   if (addToCart) {
     addToCart.addEventListener("click", (e) => {
-      e.preventDefault;
+      e.preventDefault();
       const path = window.location.pathname;
       const productId = path.split("/").pop();
       addToCartRequest(productId);
     });
   }
 
+  // TOGGLE .checked WHILE CHECKBOX BEEN CLICKED
   if (itemCheckbox) {
     itemCheckbox.forEach((item) => {
       item.addEventListener("change", () => {
@@ -31,19 +39,26 @@ export const initCart = () => {
         //- update checkout area and all-checkbox statement
         calCheckout();
         updateAllCheckbox();
+        getCheckedItem(checkedItems, item);
       });
     });
   }
 
+  // TOGGLE .checked WHILE ALL-CHECKBOX BEEN CLICKED
   if (allCheckbox) {
     allCheckbox.addEventListener("change", () => {
       //- classList.toggle("addedClass", Boolean)
       const isChecked = allCheckbox.checked;
       allCheckbox.classList.toggle("checked", isChecked);
 
+      // 清空，改以 allCheckbox .checked 為準
+      checkedItems = [];
+      checkedQty = [];
+
       itemCheckbox.forEach((item) => {
         item.checked = isChecked;
         item.classList.toggle("checked", isChecked);
+        getCheckedItem(checkedItems, item);
       });
       calCheckout();
     });
@@ -52,7 +67,9 @@ export const initCart = () => {
   if (decreaseBtns && increaseBtns) {
     document.querySelectorAll(".decrease-btn, .increase-btn").forEach((button) => {
       const increment = button.classList.contains("decrease-btn") ? -1 : 1;
-      button.addEventListener("click", () => setQuantity(button, increment));
+      button.addEventListener("click", (e) => {
+        setQuantity(button, increment);
+      });
     });
   }
 
@@ -60,9 +77,36 @@ export const initCart = () => {
     deleteItemBtn.forEach((deleteButton) => {
       deleteButton.addEventListener("click", (e) => {
         if (e.target.matches(".item-delete")) {
-          deleteItem(e.target);
+          const itemElement = e.target.closest(".item");
+          itemElement.remove(); // remove item
+          const productId = itemElement.dataset.productId;
+          deleteItem(productId);
+          calCheckout();
+          updateAllCheckbox();
         }
       });
+    });
+  }
+
+  if (checkoutBtn) {
+    checkoutBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      checkedQty = [];
+
+      const productQty = document.querySelectorAll(".qt-input");
+      productQty.forEach((quantity) => {
+        const checkboxNearest = quantity.closest(".item").querySelector(".item-checkbox");
+        if (checkboxNearest.classList.contains("checked")) {
+          checkedQty.push(parseInt(quantity.value));
+        }
+      });
+
+      if (checkedItems.length !== 0 && checkedQty.length !== 0) {
+        orderProducts(checkedItems, checkedQty);
+      } else {
+        showAlert("error", "Please select an item to checkout!");
+      }
     });
   }
 };
