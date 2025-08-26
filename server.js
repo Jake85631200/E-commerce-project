55df606cc40d647c2591e7fdf87eef93c7e2d71f
Jake85631200 <Jake85631200@gmail.com>
2025-08-26 10:59:43 +0800

    Fixing Render PORT issue

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect(DB).then(() => {
   console.log("DB connection successful!");
 });
 
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is running http://127.0.0.1:${port}`);
 });
